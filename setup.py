06a752142f9f5e9796b851d6a03dff3b237a6776
Max Bluestone <mbluestone93@gmail.com>
2020-02-18 17:23:13 -0500

    write setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name="HouseNumberDetection",
+      author="Max Bluestone",
+      author_email="mbluestone93@gmail.com",
+      url="https://github.com/mbluestone/HouseNumberDetection",
+      packages=["HND"])
